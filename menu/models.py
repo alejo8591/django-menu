246b11ed40e836335bbddc69a61b630ea3c6da50
Alejandro Romero <alejo8591@gmail.com>
2012-10-19 15:42:42 -0500

    update with flatpage field

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.flatpages.models import FlatPage
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
@@ -25,12 +26,13 @@ class Menu(models.Model):
             current += 10
 
 class MenuItem(models.Model):
-    menu = models.ForeignKey(Menu)
+    menu = models.ForeignKey('Menu')
     order = models.IntegerField()
-    link_url = models.CharField(max_length=100, help_text='URL or URI to the content, eg /about/ or http://foo.com/')
+    link_url = models.CharField(max_length=100, help_text='URL or URI to the content, eg /about/ or http://foo.com/', blank=True, null=True)
+    flatpage = models.ForeignKey(FlatPage, help_text='Select Static Page', blank=True, null=True)
     title = models.CharField(max_length=100)
     login_required = models.BooleanField(blank=True, help_text='Should this item only be shown to authenticated users?')
     anonymous_only = models.BooleanField(blank=True, help_text='Should this item only be shown to non-logged-in users?')
     
     def __unicode__(self):
-        return "%s %s. %s" % (self.menu.slug, self.order, self.title)
+        return "%s %s. %s %s" % (self.menu.slug, self.order, self.title, self.flatpage)
